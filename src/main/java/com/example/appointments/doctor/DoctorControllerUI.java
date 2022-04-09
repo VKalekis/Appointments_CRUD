00663ca469d2f6080747d075e122a4087cdc23bd
VKalekis <v.kalekis@gmail.com>
2022-04-09 16:44:53 +0300

    Crud Repository - Added logic for Doctor Entity.

---
+++ src/main/java/com/example/appointments/doctor/DoctorControllerUI.java
@@ -0,0 +1,23 @@
+package com.example.appointments.doctor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/doctors")
+public class DoctorControllerUI {
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping("/ui")
+    public String bootstrap(Model model) {
+        List<Doctor> doctorList = doctorService.getDoctors();
+        model.addAttribute("doctorList", doctorList);
+        return "doctors";
+    }
+}
